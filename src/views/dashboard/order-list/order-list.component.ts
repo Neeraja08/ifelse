1ef9a9371a3af1c5f53e67bc803ad2dc393398f1
Neeraja08 <thirupathineeraja08@gmail.com>
2023-11-09 11:11:56 +0530

    update

--- src/views/dashboard/order-list/order-list.component.ts
+++ src/views/dashboard/order-list/order-list.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-order-list',
@@ -12,6 +13,15 @@ import { Component, Input, OnInit } from '@angular/core';
 export class OrderListComponent implements OnInit {
   @Input() orders:any=[];
   constructor() {}
+  show(e:any){
+    console.log(e)
+    Swal.fire({
+      html:
+      '<h4>'+'Customer: <b style="color:#506de2">'+e.name+'</b></h4>' +
+      '<h4>'+'Product: <b style="color:#506de2">'+e.product+'</b></h4>',
+      confirmButtonText: 'OK'
+    });
+  }
 
   ngOnInit() {}
 }
